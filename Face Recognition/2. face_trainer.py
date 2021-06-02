2172497714cf3a22149444c233b7542f395ef42d
M.V.S.Sai hiranmayee <62197337+hiranmayee1123@users.noreply.github.com>
2021-06-02 12:43:17 +0530

    Add files via upload

---
+++ Face Recognition/2. face_trainer.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+import numpy as np
+from PIL import Image
+
+recognizer=cv2.face.LBPHFaceRecognizer_create()
+path='dataSet'
+def getImagesWithID(path):
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
+    faces=[]
+    IDs=[]
+
+    for imagepath in imagePaths:
+        faceImg=Image.open(imagepath).convert('L')
+        faceNp=np.array(faceImg,'uint8')
+        ID=int(os.path.split(imagepath)[-1].split(".")[1])
+        faces.append(faceNp)
+        IDs.append(ID)
+        cv2.imshow("training",faceNp)
+        cv2.waitKey(10)
+    return np.array(IDs),faces
+
+Ids,faces=getImagesWithID(path)
+recognizer.train(faces,Ids)
+recognizer.save('recognizer/trainingData.yml')
+cv2.destroyAllWindows()
+        
+
